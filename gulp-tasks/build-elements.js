9ce70d30414719f212aee4b1d8bbde77618e51b5
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-04-03 09:17:01 +0300

    Fix error handling for gulp build task

--- gulp-tasks/build-elements.js
+++ gulp-tasks/build-elements.js
@@ -8,16 +8,17 @@ var gulp = require('gulp'),
 
 
 
-module.exports = function () {
-    return gulp.src(['./src/elements/**/*.html'])
+module.exports = function (done) {
+    gulp.src(['./src/elements/**/*.html'])
         .pipe(builder([process.cwd() + '/src/bower_components/']))
         .pipe(compileHtmlTags('style', function (tag, data) {
-            return data.pipe(sass())
+            return data.pipe(sass().on('error', function(error) {console.log('\x1b[31m%s\x1b[0m', error.message); done()}))
         }))
         .pipe(compileHtmlTags('script', function (tag, data) {
             return data.pipe(babel({
                 presets: ['es2015']
-            }));
+            }).on('error', function(error) {console.log('\x1b[31m%s\x1b[0m', error.message); done()}));
         }))
         .pipe(gulp.dest('./build/elements'))
+        .on('end', function() {done();})
 };
