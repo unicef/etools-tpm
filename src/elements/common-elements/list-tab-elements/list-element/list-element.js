e41f4e68ae9d0958d59605e48309de235e7b3b7c
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-04-18 13:34:15 +0300

    improve list element

--- src/elements/common-elements/list-tab-elements/list-element/list-element.js
+++ src/elements/common-elements/list-tab-elements/list-element/list-element.js
@@ -2,14 +2,40 @@
 
 Polymer({
     is: 'list-element',
+    properties: {
+        itemValues: {
+            type: Object,
+            value: function() {
+                return {};
+            }
+        }
+    },
     _toggleRowDetails: function() {
         this.$.details.toggle();
     },
     _isLink: function(link) {
         return !!link;
     },
-    _getValue: function(fieldName) {
-        return fieldName === 'status' ? this._getStatus(this.data.vision_synced) : this.data[fieldName] || '--';
+    _getValue: function(item) {
+        let value = this.data;
+        if (!item.path) {
+            value = value[item.name] || '--';
+        } else {
+            let fields = item.path.split('.');
+
+            while (fields.length && value) {
+                value = value[fields.shift()];
+            }
+        }
+        // if (item.name === 'type' || item.name ==='status') {
+        //     value = this._refactorValue(item.name, value);
+        // }
+
+        return value || '--';
+    },
+    _refactorValue: function(type, value) {
+        let values = this.itemValues[type];
+        if (values) { return values[value]; }
     },
     _getStatus: function(synced) {
         if (synced) { return 'Synced from VISION'; }
@@ -18,6 +44,7 @@ Polymer({
         return value || '--';
     },
     _getLink: function(pattern) {
-        return pattern.replace('*data_id*', this.data.id);
+        return pattern
+            .replace('*data_id*', this.data.id);
     }
 });
