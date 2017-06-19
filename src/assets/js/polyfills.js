738830d5ed4b6c190fd2829a3c96a923b244772f
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-06-19 15:04:36 +0300

    Add new polyfill

--- src/assets/js/polyfills.js
+++ src/assets/js/polyfills.js
@@ -43,3 +43,15 @@ if (!Array.prototype.find) {
         return undefined;
     };
 }
+
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function(searchString, position) {
+            position = position || 0;
+            return this.indexOf(searchString, position) === position;
+        }
+    });
+}
