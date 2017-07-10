bf8ee280585147eb49344998cf32a8a4e6e652f5
Bogdan Poslovsky <bogdan.p@razortheory.com>
2017-07-10 20:25:01 +0300

    Move Staff Members from AP

--- src/elements/pages/partners-page-components/partner-info-view/partner-staff-members-tab/partner-staff-members-tab.js
+++ src/elements/pages/partners-page-components/partner-info-view/partner-staff-members-tab/partner-staff-members-tab.js
@@ -4,97 +4,355 @@ Polymer({
     is: 'partner-staff-members-tab',
 
     behaviors: [
-        TPMBehaviors.RepeatableDataSetsBehavior,
-        TPMBehaviors.PermissionController
+        TPMBehaviors.TableElementsBehavior,
+        TPMBehaviors.CommonMethodsBehavior
     ],
-    properties: {},
-
-    ready: function() {
-        this.dataSetModel =  {
-            user: {
-                first_name: '',
-                last_name: '',
-                email: '',
-                is_active: true,
-                profile: {
-                    job_title: '',
-                    phone_number: ''
-                }
-            },
-            receive_audit_notifications: false
-        };
-
-        this.$['email-validator'].validate = this._validEmailAddress.bind(this);
+    properties: {
+        mainProperty: {
+            type: String,
+            value: 'staff_members'
+        },
+        vendorNumber: {
+            type: String,
+            observer: '_changeVendorNumber'
+        },
+        dataItems: {
+            type: Array,
+            value: function() {
+                return [];
+            }
+        },
+        itemModel: {
+            type: Object,
+            value: function() {
+                return {
+                    user: {
+                        first_name: '',
+                        last_name: '',
+                        email: '',
+                        profile: {
+                            job_title: '',
+                            phone_number: ''
+                        },
+                        isActive: true,
+                        isNotify: true
+                    }
+                };
+            }
+        },
+        columns: {
+            type: Array,
+            value: function() {
+                return [
+                    {
+                        'size': 18,
+                        'label': 'Position',
+                        'name': 'user.profile.job_title'
+                    }, {
+                        'size': 18,
+                        'label': 'First Name',
+                        'name': 'user.first_name'
+                    }, {
+                        'size': 18,
+                        'label': 'Last Name',
+                        'name': 'user.last_name'
+                    }, {
+                        'size': 18,
+                        'label': 'Phone Number',
+                        'name': 'user.profile.phone_number'
+                    }, {
+                        'size': 18,
+                        'label': 'E-mail Address',
+                        'name': 'user.email'
+                    }, {
+                        'size': 18,
+                        'label': 'Active',
+                        'name': 'user.is_active'
+                    },{
+                        'size': 10,
+                        'label': 'Active',
+                        'name': 'user.is_active'
+                    },
+                    {
+                        'size': 10,
+                        'label': 'Notify',
+                        'name': 'receive_tpm_notifications'
+                    }
+                ];
+            }
+        },
+        addDialogTexts: {
+            type: Object,
+            value: function() {
+                return {
+                    title: 'Add new Staff Member'
+                };
+            }
+        },
+        editDialogTexts: {
+            type: Object,
+            value: function() {
+                return {
+                    title: 'Edit Staff Member'
+                };
+            }
+        },
+        listQueries: {
+            type: Object,
+            value: function() {
+                return {
+                    page: 1,
+                    page_size: 10
+                };
+            }
+        },
+        listLoading: {
+            type: Boolean,
+            value: false
+        },
+        showingResults: {
+            type: String,
+            computed: '_calcShowingResults(datalength, listSize, listPage, searchQuery, dataItems.length)'
+        },
+        datalength: {
+            type: Number,
+            value: 0
+        },
+        searchQuery: {
+            type: String,
+            value: ''
+        }
     },
 
-    _canBeRemoved: function() {
-        if (!this.basePermissionPath) { return true; }
-
-        let readOnly = this.isReadonly(`${this.basePermissionPath}.staff_members`);
-        if (readOnly === null) { readOnly = true; }
-
-        return !readOnly;
+    listeners: {
+        'dialog-confirmed': '_addStaffFromDialog',
+        'delete-confirmed': 'removeStaff',
+        'staff-updated': '_staffUpdated'
     },
 
-    _validEmailAddress: function(emailAddress) {
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return !!(emailAddress && re.test(emailAddress));
+    observers: [
+        'resetDialog(dialogOpened)',
+        '_handleUpdateError(errorObject.staff_members)',
+        '_queriesChanged(listSize, listPage, searchQuery)',
+        'updateStyles(emailChecking, staffsBase, addDialog)'
+    ],
+
+    attached: function() {
+        this.$.emailInput.validate = this._validEmailAddress.bind(this, this.$.emailInput);
+        this.listSize = 10;
+        this.listPage = 1;
     },
 
-    _notEmpty: function(value) {
-        return typeof value !== 'undefined' && value !== null && value !== '';
+    _changeVendorNumber: function(id) {
+        if (!this._canBeChanged() || !this.basePermissionPath) { return; }
+        if (!id) { this.resetList(); }
+    },
+
+    _queriesChanged: function(listSize, listPage, searchQuery) {
+        if (!listPage || !listSize) { return; }
+
+        if (((this.lastSize && this.lastSize !== listSize) ||
+            (!_.isUndefined(this.lastSearchQuery) && this.lastSearchQuery !== searchQuery)) && this.listPage !== 1) {
+            this.lastSearchQuery = searchQuery;
+            this.lastSize = listSize;
+            this.listPage = 1;
+            return;
+        }
+        this.lastSize = listSize;
+        this.lastSearchQuery = searchQuery;
+
+        this.set('listQueries', {
+            page_size: listSize,
+            page: listPage,
+            search: searchQuery || ''
+        });
     },
 
     validate: function() {
-        if (!this.dataItems.length) { return true; }
-
-        let elements = Polymer.dom(this.root).querySelectorAll('.validate-input'),
-            valid = true;
+        let elements = Polymer.dom(this.root).querySelectorAll('.validate-input:not(.email)'),
+            valid = true,
+            emailValid = this.$.emailInput.disabled || this.$.emailInput.validate();
 
         Array.prototype.forEach.call(elements, (element) => {
-            //TODO: improve validation
-            if (element.required && !element.validate()) { valid = false; }
+            if (element.required && !element.disabled && !element.validate()) {
+                element.invalid = 'This field is required';
+                element.errorMessage = 'This field is required';
+                valid = false;
+            }
         });
+
+        return valid && emailValid;
+    },
+
+    _calcShowingResults: function(datalength, listSize, listPage, searchQuery, itemsLength) {
+        let last = listSize * listPage,
+            first = last - listSize + 1,
+            length = searchQuery ? itemsLength : datalength;
+
+        if (last > length) { last = length; }
+        if (first > length) { first = 0; }
+        return `${first}-${last} of ${length}`;
+    },
+
+    _validEmailAddress: function(emailInput) {
+        let value = emailInput.value,
+            required = emailInput.required;
+
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (required && !value) {
+            this.errors = {user: {email: 'Email is required'}};
+            return false;
+        }
+        if (value && !re.test(value)) {
+            this.errors = {user: {email: 'Email is incorrect'}};
+            return false;
+        }
+
+        let valid = true;
+
+        if (this.saveWithButton) {
+            _.each(this.dataItems, item => {
+                if (item.user && item.user.email === this.editedItem.user.email &&
+                    item.id && item.id === this.editedItem.id) {
+                    this.errors = {user: {email: 'Email must be unique'}};
+                    valid = false;
+                }
+            });
+        }
 
         return valid;
     },
 
-    _addNewStaffMember: function() {
-        if (this._canBeRemoved()) {
-            var lastStaffMemberAdded = this.dataItems[this.dataItems.length - 1];
-            if (lastStaffMemberAdded &&
-                !this._notEmpty(lastStaffMemberAdded.user.profile.job_title) &&
-                !this._notEmpty(lastStaffMemberAdded.user.first_name) &&
-                !this._notEmpty(lastStaffMemberAdded.user.last_name) &&
-                !this._notEmpty(lastStaffMemberAdded.user.profile.phone_number) &&
-                !this._notEmpty(lastStaffMemberAdded.user.email)) {
-                this.fire('toast', {text: 'Last staff member fields are empty!', showCloseBtn: true});
-            } else {
-                this._addElement();
-            }
+    _emailDisabled: function(request, createPopup, emailChecking) {
+        return !!(!createPopup || request || emailChecking);
+    },
+
+    _checkEmail: function(event) {
+        if (this.emailChecking) { return; }
+
+        let input = event && event.target,
+            value = input && input.value;
+
+        if (value && this._validEmailAddress(input)) {
+            this.newEmail = value;
         }
     },
 
-    _getTitleValue: function(value) { return value || ''; },
-
-    _setRequired: function(field) {
-        if (!this.basePermissionPath) { return false; }
-
-        let required = this.isRequired(`${this.basePermissionPath}.staff_members.${field}`);
-
-        return required ? 'required' : false;
+    _showPagination: function(dataItems) {
+        return !!(+dataItems && +dataItems > 10);
     },
 
-    _resetFieldError: function(event) {
-        event.target.invalid = false;
+    _staffLength: function(length, length2, search) {
+        let staffLength = search ? length2 : length || length2;
+        return staffLength || 0;
     },
-    isReadOnly: function(field) {
-        if (!this.basePermissionPath) { return true; }
 
-        let readOnly = this.isReadonly(`${this.basePermissionPath}.staff_members.${field}`);
-        if (readOnly === null) { readOnly = true; }
+    _addStaffFromDialog: function() {
+        if (this.requestInProcess) { return; }
 
-        return readOnly;
+        if (!this.validate()) { return; }
+
+        this.requestInProcess = true;
+
+        let item = _.cloneDeep(this.editedItem);
+        if (!this.addDialog && !isNaN(this.editedIndex)) {
+            if (_.isEqual(this.originalEditedObj, this.editedItem)) {
+                this.requestInProcess = false;
+                this.dialogOpened = false;
+                this.resetDialog();
+                return;
+            }
+            this.set('newData', {
+                method: 'PATCH',
+                data: item,
+                staffIndex: this.editedIndex,
+                id: `${item.id}/`
+            });
+        } else {
+            this.set('newData', {
+                method: 'POST',
+                data: item,
+                id: ''
+            });
+        }
+    },
+
+    removeStaff: function() {
+        this.requestInProcess = true;
+        this.set('newData', {
+            method: 'DELETE',
+            data: {},
+            staffIndex: this.editedIndex,
+            id: `${this.editedItem.id}/`
+        });
+    },
+
+    _staffUpdated: function(event, details) {
+        if (!details) { throw 'Detail are not provided!'; }
+        if (details.error) {
+            this._handleUpdateError(details.errorData);
+            return;
+        }
+
+        if (details.action === 'patch') {
+            this.splice('dataItems', details.index, 1, details.data);
+        } else if (details.action === 'post') {
+            this.set('listPage', 0);
+            this.set('listPage', 1);
+        } else if (details.action === 'delete') {
+            let last = this.dataItems.length === 1 ? 1 : 0;
+            this.set('listQueries', {
+                page_size: this.listSize,
+                page: this.listPage - last || 1
+            });
+        }
+        this.requestInProcess = false;
+        this.dialogOpened = false;
+        this.resetDialog();
+    },
+
+    _handleUpdateError: function(errorData) {
+        let nonField = this.checkNonField(errorData);
+        let error =  this.refactorErrorObject(errorData);
+
+        this.set('errors', error);
+        this.requestInProcess = false;
+        if (_.isString(error)) {
+            let text = !!~error.indexOf('required') ? 'Please, select at least one staff member.' : error;
+            this.fire('toast', {text: `Staff Members: ${text}`});
+        }
+        if (nonField) {
+            this.fire('toast', {text: `Staff Members: ${nonField}`});
+        }
+    },
+
+    resetList: function() {
+        this.set('dataItems', []);
+        this.set('listPage', 1);
+        this.set('searchQuery', '');
+        this.set('searchString', '');
+        this.set('datalength', 0);
+        this.updateStyles();
+    },
+
+    _getSearchInputClass: function(searchString) {
+        if (searchString) { return 'filled'; }
+        return 'empty';
+    },
+
+    searchBlur: function() {
+        this.updateStyles();
+    },
+
+    _searchChanged: function() {
+        let value = this.$.searchInput.value || '';
+
+        if (value.length - 1) {
+            this.debounce('newRequest', () => {
+                this.set('searchQuery', value);
+            }, 500);
+        }
     }
 
 });
