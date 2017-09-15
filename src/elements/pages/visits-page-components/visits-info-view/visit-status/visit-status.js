c65073e93e8370362523b92b81c69f3011d4d25c
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-09-15 17:24:30 +0300

    Start to refactor statuses

--- src/elements/pages/visits-page-components/visits-info-view/visit-status/visit-status.js
+++ src/elements/pages/visits-page-components/visits-info-view/visit-status/visit-status.js
@@ -8,14 +8,32 @@ Polymer({
         TPMBehaviors.StaticDataController
     ],
 
-    properties: {},
-
-    ready: function() {
-        this.statuses = this.getData('statuses');
+    properties: {
+        dateProperties: {
+            type: Object,
+            value: function() {
+                return {};
+            }
+        }
     },
 
-    _getStatusClass: function(currentStatus, status, statuses) {
-        if (!currentStatus || !status) { return; }
+    observers: ['_setCancelledPosition(visit.status)'],
+
+    ready: function() {
+        this.statuses = this.getData('statuses') || [];
+        _.each(this.statuses, (status) => {
+            if (status.value === 'draft') {
+                this.set(`dateProperties.draft`, 'date_created');
+            } else {
+                this.set(`dateProperties.${status.value}`, `date_of_${status.value}`);
+            }
+        })
+    },
+
+    _getStatusClass: function(visit, status, statuses) {
+        if (!visit || !visit.status || !status) { return; }
+        let currentStatus = visit.status,
+            statusDateField = this.dateProperties[status];
 
         if (status === 'tpm_rejected') {
             return 'rejected';
@@ -25,7 +43,7 @@ Polymer({
             return 'report_rejected';
         } else if (currentStatus === status) {
             return 'active';
-        } else if (this._getStatusIndex(currentStatus, statuses) > this._getStatusIndex(status, statuses)) {
+        } else if (visit[statusDateField]) {
             return 'completed';
         } else {
             return 'pending';
@@ -36,13 +54,10 @@ Polymer({
         if (!status || !statuses) { return; }
         if (!this.statusesArray) { this.statusesArray = statuses.map((status) => { return status.value; }); }
 
-        let currentIndex = this.statusesArray.indexOf(status);
-        if (currentIndex > this.statusesArray.indexOf('tpm_accepted') ||
-            currentIndex > this.statusesArray.indexOf('tpm_rejected')) {
-            currentIndex -= 1;
-        }
+        let currentIndex = this.statusesArray.indexOf(status),
+            statusIndex = Math.ceil(currentIndex/2);
 
-        return currentIndex >= 0 ? currentIndex : -1;
+        return statusIndex >= 0 ? statusIndex : -1;
     },
 
     setIndex: function(status, statuses) {
@@ -61,6 +76,21 @@ Polymer({
     hideDivider: function(status, statuses) {
         let lastStatus = statuses[statuses.length - 1];
         return !!(lastStatus && lastStatus.value === status);
+    },
+
+    _setCancelledPosition: function(status) {
+        if (!status || status !== 'cancelled') { return; }
+        let cancelledStatus = this.splice('statuses', this.statuses.findIndex(status => status.value === 'cancelled'), 1);
+        let lastActiveIndex;
+        _.forEachRight(this.statuses, (status, index) => {
+            if (lastActiveIndex) { return; }
+            let statusDateField = this.dateProperties[status.value];
+            if (this.visit[statusDateField]) {
+                lastActiveIndex = index;
+            }
+        });
+
+        this.splice('statuses', lastActiveIndex + 1, 0, cancelledStatus);
     }
 
 });
