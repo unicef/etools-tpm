4648c300e508c5db98c3d8343807ce520827d9ed
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-09-15 15:02:14 +0300

    Add temporary gulp task

--- gulp-tasks/copy-bower.js
+++ gulp-tasks/copy-bower.js
@@ -1,13 +1,16 @@
 'use strict';
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    patchElement = require('./patch-element');
 
 function copyBowerComponents() {
     return gulp.src(['./bower_components/**/*'], {since: gulp.lastRun(copyBowerComponents)})
+        .pipe(patchElement)
         .pipe(gulp.dest('./build/bower_components/'));
 }
 
 function copyBowerToSrc() {
     return gulp.src(['./bower_components/**/*'], {since: gulp.lastRun(copyBowerToSrc)})
+        .pipe(patchElement)
         .pipe(gulp.dest('./src/bower_components/'));
 }
 
