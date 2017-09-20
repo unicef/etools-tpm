1bad4151cc4129e782ae6084dc331384215df355
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-09-20 08:50:11 +0300

    Remove redundant gulp task

--- gulp-tasks/copy-bower.js
+++ gulp-tasks/copy-bower.js
@@ -1,16 +1,13 @@
 'use strict';
-var gulp = require('gulp'),
-    patchElement = require('./patch-element');
+var gulp = require('gulp');
 
 function copyBowerComponents() {
     return gulp.src(['./bower_components/**/*'], {since: gulp.lastRun(copyBowerComponents)})
-        .pipe(patchElement)
         .pipe(gulp.dest('./build/bower_components/'));
 }
 
 function copyBowerToSrc() {
     return gulp.src(['./bower_components/**/*'], {since: gulp.lastRun(copyBowerToSrc)})
-        .pipe(patchElement)
         .pipe(gulp.dest('./src/bower_components/'));
 }
 
