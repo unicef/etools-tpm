192561c65675dab1326d4f72e9d04a8ca9ba3897
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-01-11 14:33:04 +0300

    Fix path normalization in gulp task

--- gulp-tasks/build-elements.js
+++ gulp-tasks/build-elements.js
@@ -49,7 +49,7 @@ function buildElements(done) {
                     }).on('error', function(error) {console.log('\x1b[31m%s\x1b[0m', error.message); done()}));
                 }),
                 through2(function(file, enc, callback){
-                    file.base = path.normalize(file.base + '..');
+                    file.base = path.normalize(file.base + '/..');
                     callback(null, file);
                 })
             )
