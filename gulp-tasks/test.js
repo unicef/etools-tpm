9dfb29f1dbacfd2372e1ced282bcf722861902dd
Roman Karpovich <fpm.th13f@gmail.com>
2018-08-09 11:18:11 +0300

    circleci 2.0 config

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -23,12 +23,27 @@ module.exports = function testElements(done) {
 
         let tests;
         if (argv.noxvfb) {
-            tests = spawn('node', ['./node_modules/.bin/wct', '--skip-selenium-install', `./build/tests/${indexFileName}`]);
+            tests = spawn('node', ['./node_modules/.bin/wct', `./build/tests/${indexFileName}`]);
         } else {
-            tests = spawn('xvfb-run', ['-a', './node_modules/.bin/wct', '--skip-selenium-install', `./build/tests/${indexFileName}`]);
+            tests = spawn('xvfb-run', ['-a', './node_modules/.bin/wct', `./build/tests/${indexFileName}`]);
         }
 
         console.log(`\x1b[32mRunning ${indexFileName}.\x1b[0m`);
+
+        tests.stdout.on('data', (data) => {
+            if (~data.indexOf('✖') || ~data.indexOf('Tests failed')) {
+                data = `\x1b[31m${data}\x1b[0m`;
+                withErrors = true;
+            } else if (~data.indexOf('ended with great success') || data.indexOf('404') === 0) {
+                return;
+            }
+            console.log(`${data}`);
+        });
+
+        tests.stderr.on('data', (data) => {
+            console.log(`\x1b[31m${data}\x1b[0m`);
+            withErrors = true;
+        });
 
         tests.stdout.on('data', (data) => {
             if (~data.indexOf('✖') || ~data.indexOf('Tests failed')) {
