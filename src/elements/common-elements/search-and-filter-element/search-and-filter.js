3cf7389c755ed2cd94f5f2691a21ad8b9dcdb2f8
Marko <marko.barca10@gmail.com>
2018-10-12 11:33:27 -0400

    adds multi select filters

--- src/elements/common-elements/search-and-filter-element/search-and-filter.js
+++ src/elements/common-elements/search-and-filter-element/search-and-filter.js
@@ -50,13 +50,16 @@
             }, 300);
         },
 
+        _isSelected: function(filter, _) {
+            const query = typeof filter === 'string' ? filter : filter.query;
+            return this.usedFilters.findIndex(usedFilter => usedFilter.query === query) !== -1;
+        },
+
         addFilter: function(e) {
             let query = (typeof e === 'string') ? e : e.model.item.query;
-            let alreadySelected = this.usedFilters.findIndex((filter) => {
-                return filter.query === query;
-            });
+            let isSelected = this._isSelected(query);
 
-            if (alreadySelected === -1) {
+            if (!isSelected) {
                 let newFilter = this.filters.find((filter) => {
                     return filter.query === query;
                 });
@@ -69,6 +72,8 @@
                     queryObject[query] = true;
                     this.updateQueries(queryObject);
                 }
+            } else {
+                this.removeFilter(e);
             }
         },
 
@@ -105,8 +110,6 @@
                     return;
                 }
 
-                let availableFilters = [];
-
                 this.filters.forEach((filter) => {
                     let usedFilter = this.usedFilters.find(used => used.query === filter.query);
 
@@ -114,16 +117,15 @@
                         this.addFilter(filter.query);
                     } else if (queryParams[filter.query] === undefined) {
                         this.removeFilter(filter.query);
-                        availableFilters.push(filter);
                     }
                 });
-                this.set('availableFilters', availableFilters);
 
                 if (queryParams.search) {
                     this.set('searchString', queryParams.search);
                 } else {
                     this.set('searchString', '');
                 }
+                this.set('availableFilters', _.clone(this.filters));
             }, 50);
         },
 
@@ -183,11 +185,13 @@
             if (detail.selectedValues && query) {
                 let filter = this._getFilter(query);
                 let optionValue = filter.optionValue || 'value';
-                let queryObject = {page: '1'};
-                queryObject[query] = detail.selectedValues[optionValue];
+                let queryObject = { page: '1' };
+                queryObject[query] = detail.selectedValues.
+                    map(val => val[optionValue]).
+                    join(',');
 
                 this.updateQueries(queryObject);
             }
-        }
+        },
     });
 })();
