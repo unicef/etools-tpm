a50c46f64e61466482a9f6ac408aac66df111204
Marko Bilal <marko.barca10@gmail.com>
2018-10-29 14:08:02 -0400

    test again

--- wct.conf.js
+++ wct.conf.js
@@ -1,21 +1,10 @@
 module.exports = {
   plugins: {
     local: {
-      'skipSeleniumInstall': true,
-      'disabled': true,
+      // browsers: ["chrome", ]
+      browsers: ["firefox"],
     },
-    "headless": {
-      "browsers": [
-        "chrome"
-      ],
-      "browsersOptions": {
-        "chrome": [
-          "window-size=1920,1080",
-          "headless",
-          "disable-gpu",
-          "no-sandbox"
-        ]
-      }
-    }
+    sauce: false,
   },
+  skipUpdateCheck: true
 };
